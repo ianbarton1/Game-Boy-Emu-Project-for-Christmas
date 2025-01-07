80363f9f6df7d79138e7ef5ed83ea084569a4418
Ian Barton <ian.barton1990@gmail.com>
2025-01-07 00:34:26 +0000

    added debug flag to main gameboy object

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from time import perf_counter
 
 blargg_test_list = [
     # 'roms/tests/blargg/01-special.gb', #hang fail
-    # 'roms/tests/blargg/02-interrupts.gb', #test 2 fail
+    'roms/tests/blargg/02-interrupts.gb', #test 2 fail
     # R'roms\tests\blargg\03-op sp,hl.gb' #crash fail
     # R"roms\tests\blargg\04-op r,imm.gb", #hang fail
     # R"roms\tests\blargg\05-op rp.gb", #hang fail
@@ -21,7 +21,7 @@ blargg_test_list = [
 ]
 
 for rom_file in blargg_test_list:
-    game_boy:GameBoy = GameBoy(cpu_debug_print_counter= None, rom_file = rom_file)
+    game_boy:GameBoy = GameBoy(cpu_debug_print_counter= None, rom_file = rom_file, display_debug= True)
     print(perf_counter())
 
     game_boy.play()
